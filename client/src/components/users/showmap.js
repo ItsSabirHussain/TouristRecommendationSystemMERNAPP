78723d860ad2bde6cc3e491ecdca07f1bc8541a7
Sabir Hussain <email.sabirhussain@gmail.com>
2019-09-21 20:33:42 +0500

    initial

--- client/src/components/users/showmap.js
+++ client/src/components/users/showmap.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { compose } from "recompose";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
 import {
   withScriptjs,
   withGoogleMap,
@@ -35,23 +38,116 @@ const MapWithAMarker = compose(
   );
 });
 
+const useStyles = makeStyles(theme => ({
+  submit: {
+    margin: theme.spacing(1, 1, 1)
+  },
+  root: {
+    display: "flex"
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: 36
+  },
+  menuButtonHidden: {
+    display: "none"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: "hidden",
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9)
+    }
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto"
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column"
+  },
+  fixedHeight: {
+    height: 240
+  }
+}));
 export default function Map() {
+  const classes = useStyles();
   const lat = localStorage.getItem("lat");
   const lng = localStorage.getItem("lng");
 
   return (
-    <MapWithAMarker
-      markers={[
-        {
-          id: 0,
-          latitude: Number(lat),
-          longitude: Number(lng)
-        }
-      ]}
-      googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-      loadingElement={<div style={{ height: `100%` }} />}
-      containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />}
-    />
+    <main className={classes.content}>
+      <div className={classes.appBarSpacer} />
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Container component="main" className={classes.main} maxWidth="sm">
+              <MapWithAMarker
+                markers={[
+                  {
+                    id: 0,
+                    latitude: Number(lat),
+                    longitude: Number(lng)
+                  }
+                ]}
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            </Container>
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
   );
 }
